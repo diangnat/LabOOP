9f459c7016bff2f36bd08ccce3f7a33cf3aea759
diangnat <hnatiuk.dian@chnu.edu.ua>
2024-04-02 10:05:57 +0300

    Create stream insertion and stream extraction and this pointer

---
+++ Customer.h
@@ -0,0 +1,48 @@
+//
+// Created by user on 01.03.2024.
+//
+
+#ifndef LABA2_3_CUSTOMER_H
+#define LABA2_3_CUSTOMER_H
+#include <iostream>
+#include <string.h>
+#include <fstream>
+using namespace std;
+
+#endif //LABA2_3_CUSTOMER_H
+
+
+class Customer {
+private:
+    string name;
+    string phoneNumber;
+    string address;
+public:
+    Customer(string n);
+
+    Customer(string n, string phone);
+
+    Customer(string n, string phone, string addr);
+
+    void setName(string n);
+
+    void setPhoneNumber(string phone);
+
+    void setAddress(string addr);
+
+    void displayInfo();
+
+    // copy constructor
+//    Customer(const Customer &other);
+
+//stream insertion
+friend ostream& operator<<(ostream& os, const Customer& customer);
+
+//stream  extraction
+friend istream& operator>>(istream& is, Customer& customer);
+
+    //this
+    Customer(string &name, string &phoneNumber, string &address);
+
+    ~Customer();
+};
---
+++ customer.cpp
@@ -0,0 +1,66 @@
+//
+// Created by user on 01.03.2024.
+//
+#include <iostream>
+#include <string>
+#include "Customer.h"
+
+using namespace std;
+
+Customer:: Customer(string n):name(n), phoneNumber("none"), address("none") {}
+
+Customer:: Customer(string n, string phone) : name(n), phoneNumber(phone) {}
+
+Customer:: Customer(string n, string phone, string addr) : name(n), phoneNumber(phone), address(addr) {}
+
+Customer::~Customer() {}
+
+void Customer:: setName (string n) {name = n; }
+
+void Customer::setPhoneNumber(string phone) {phoneNumber = phone; }
+
+void Customer:: setAddress(string addr) {address = addr; }
+
+void Customer:: displayInfo(){
+    cout << "name " << name <<'\n'
+         <<"phone number "<<phoneNumber<<'\n'
+         <<"address "<<address<<'\n';
+
+}
+// copy constructor(shallow copy)
+//Customer::Customer(const Customer &other){
+//    name = other.name;
+//    phoneNumber = other.phoneNumber;
+//    address = other.address;
+//}
+
+
+//this
+Customer::Customer(string &name, string &phoneNumber, string &address) {
+    this->name = name;
+    this->phoneNumber = phoneNumber;
+    this->address = address ;
+    this->name =name;
+    this->phoneNumber = phoneNumber;
+    this->address = address;
+}
+
+//stream insertion
+
+ostream &operator<<(std::ostream &os, const Customer &customer) {
+    os << "Name: "<<customer.name<<endl;
+    os << "Phone number: "<<customer.phoneNumber<<endl;
+    os<< "Address: "<<customer.address<<endl;
+    return os;
+}
+
+//stream  extraction
+istream& operator>>(istream& is, Customer& customer){
+    cout<<"Enter name: "<<endl;
+    is>>customer.name;
+    cout<<"Enter phone number: "<<endl;
+    is>>customer.phoneNumber;
+    cout<<"Enter address: "<<endl;
+    is>>customer.address;
+}
+
